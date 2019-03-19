c351ff807735152b70078494376ba52f65f3f79b
Elytrus <moses@mosesxu.net>
2019-03-19 02:25:24 -0400

    correct art gallery problem solution

--- cpp/dmopc18c5p4.cpp
+++ cpp/dmopc18c5p4.cpp
@@ -1,8 +1,8 @@
 //============================================================================
 // Name        : dmopc18c5p4.cpp
-// Author      : Daxi the Taxi
+// Author      : 
 // Version     :
-// Copyright   : ALL YOUR CODE IS BELONG TO US
+// Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
@@ -11,85 +11,42 @@
 using namespace std;
 
 typedef long long ll;
-typedef pair<int, int> p;
 
-const int MAX = 200001;
-int n, para = 0, parb = 0;
-string a, b;
-bool diffs[MAX];
+int n;
+string a, b, basea, baseb;
 
-#define YES printf("YES\n")
-#define NO printf("NO\n")
+inline void base(string &begin, string &base){
+	deque<int> fs;
 
-inline int parity(string &s){
-	int cnt = 0;
-	for(int i = 0; i < n; i++)
-		cnt += s[i] == 'F';
-	return cnt % 2;
-}
+	for (int i = 0; i < n; ++i) {
+		if(begin[i] == 'F'){
+			if(!fs.empty() && fs.back() % 2 != i % 2){
+				fs.pop_back();
+				continue;
+			}
 
-inline bool alldiff(string &s){
-	for(int i = 1; i < n; i++){
-		if(s[i] == s[i - 1])
-			return false;
+			if(!fs.empty())
+				fs.push_back(fs.back() + 2);
+			else
+				fs.push_back(i % 2);
+		}
 	}
-	return true;
+
+	base.resize(n);
+	fill(base.begin(), base.end(), 'A');
+	for(int i : fs)
+		base[i] = 'F';
 }
 
 int main(){
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+	cin.tie(0);
 
 	cin >> n >> a >> b;
 
-	if(n == 1 || alldiff(a)){
-		if(a == b)
-			YES;
-		else
-			NO;
+	base(a, basea);
+	base(b, baseb);
 
-		return 0;
-	}
-
-	if(parity(a) != parity(b)){
-		NO;
-	}
-	else{
-		YES;
-	}
-
-//	for (int i = 0; i < n; ++i)
-//		diffs[i] = a[i] != b[i];
-//
-//	printf("diffs: ");
-//		for (int i = 0; i < n; ++i) {
-//			printf("%d, ", diffs[i]);
-//		}
-//		printf("\n");
-//
-//	for (int i = 1; i < n; i++)
-//		if(diffs[i - 1] && diffs[i])
-//			diffs[i - 1] = diffs[i] = false;
-//
-//	int dcnt = 0;
-//	for (int i = 0; i < n; ++i)
-//		dcnt += diffs[i];
-//
-//	if(dcnt != 2){
-//		NO;
-//		return 0;
-//	}
-//
-//	int first = -1, second = -1;
-//	for (int i = 0; i < n; ++i) {
-//		if(diffs[i]){
-//			if(first == -1) first = i;
-//			else if(second == -1) second = i;
-//		}
-//	}
-//
-//	if((second - first) % 2)
-//		YES;
-//	else
-//		NO;
+	if(basea == baseb) printf("YES\n");
+	else printf("NO\n");
 }
